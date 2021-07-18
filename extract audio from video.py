db49d73ba4bc02844c6f3500b42a6f6068106dcb
sanjay0001 <75019244+sanjay0001@users.noreply.github.com>
2021-07-18 20:17:17 +0530

    Update extract audio from video.py

--- extract audio from video.py
+++ extract audio from video.py
@@ -1,7 +1,12 @@
 import pafy
-url=""#video url link
-FOLDER=""#path for downloaded audio
-video=pafy.new(url)
-audios=video.audiostreams
-best_audio=video.getbestaudio()
-best_audio.download(filepath = FOLDER)
+def extract_audio(url):
+    FOLDER=""#path for downloaded audio
+    video=pafy.new(url)
+    audios=video.audiostreams
+    best_audio=video.getbestaudio()
+    best_video=video.getbest()
+    best_audio.download(filepath = FOLDER)
+    best_video.download(filepath=FOLDER)
+    return FOLDER
+
+#test url:https://www.youtube.com/watch?v=klyJChzMvtQ
